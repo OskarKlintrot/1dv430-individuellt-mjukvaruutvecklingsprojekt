64dbf7449c52e0aead8f4773bb51fc93b3667227
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-04-02 21:21:45 +0200

    Possible to turn led on and off via serial

--- Serial/Serial.ino
+++ Serial/Serial.ino
@@ -13,22 +13,33 @@ const long interval = 1000;           // interval at which to blink (millisecond
 long randNumber;
 
 void setup(){
-	Serial.begin(9600);
+  
+  Serial.begin(9600);
+  
+  //if analog input pin 0 is unconnected, random analog
+  //noise will cause the call to randomSeed() to generate
+  //different seed numbers each time the sketch runs.
+  //randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
 
-	// if analog input pin 0 is unconnected, random analog
-	// noise will cause the call to randomSeed() to generate
-	// different seed numbers each time the sketch runs.
-	// randomSeed() will then shuffle the random function.
-	randomSeed(analogRead(0));
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // send data only when you receive data:
   if (Serial.available() > 0) {
     incomingSerial = Serial.readStringUntil('#');
+    
+    incomingSerial.trim();
   
     Serial.print("I received: ");
     Serial.println(incomingSerial);
+    
+    if (incomingSerial == "HIGH")
+        digitalWrite(13, HIGH);
+    else if (incomingSerial == "LOW")
+        digitalWrite(13, LOW);
   }
  
 
