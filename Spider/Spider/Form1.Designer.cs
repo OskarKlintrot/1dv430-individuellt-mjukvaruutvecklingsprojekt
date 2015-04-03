d00872afb73d7f8da7f34d9a62a2ebccd51b54eb
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-04-03 12:14:02 +0200

    Basic On/Off functionality implemented

---
+++ Spider/Spider/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Spider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.On_btn = new System.Windows.Forms.Button();
+            this.Off_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // On_btn
+            // 
+            this.On_btn.Location = new System.Drawing.Point(34, 90);
+            this.On_btn.Name = "On_btn";
+            this.On_btn.Size = new System.Drawing.Size(75, 23);
+            this.On_btn.TabIndex = 0;
+            this.On_btn.Text = "On";
+            this.On_btn.UseVisualStyleBackColor = true;
+            this.On_btn.Click += new System.EventHandler(this.On_btn_Click);
+            // 
+            // Off_btn
+            // 
+            this.Off_btn.Location = new System.Drawing.Point(163, 90);
+            this.Off_btn.Name = "Off_btn";
+            this.Off_btn.Size = new System.Drawing.Size(75, 23);
+            this.Off_btn.TabIndex = 0;
+            this.Off_btn.Text = "Off";
+            this.Off_btn.UseVisualStyleBackColor = true;
+            this.Off_btn.Click += new System.EventHandler(this.Off_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.Off_btn);
+            this.Controls.Add(this.On_btn);
+            this.Name = "Form1";
+            this.Text = "SerialDBSpider";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button On_btn;
+        private System.Windows.Forms.Button Off_btn;
+    }
+}
+
---
+++ Spider/Spider/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Spider
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort myport;
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void On_btn_Click(object sender, EventArgs e)
+        {
+            myport.WriteLine("HIGH");
+
+            On_btn.Enabled = false;
+            Off_btn.Enabled = true;
+        }
+        private void Off_btn_Click(object sender, EventArgs e)
+        {
+            myport.WriteLine("LOW");
+
+            On_btn.Enabled = true;
+            Off_btn.Enabled = false;
+        }
+            private void init()
+        {
+            try
+            {
+                myport = new SerialPort();
+                myport.BaudRate = 9600;
+                myport.PortName = "COM4";
+                myport.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong when trying to connect to the Arduino");
+            }
+
+                On_btn.Enabled = true;
+                Off_btn.Enabled = false;
+        }
+
+    }
+}
