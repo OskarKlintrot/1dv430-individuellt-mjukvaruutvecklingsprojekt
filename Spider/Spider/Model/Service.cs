aa8848ec6e540eb35522a813277b0fb22975bcc2
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-04-06 19:43:48 +0200

    Created a service-class. No validation though.

--- Spider/Spider/Model/Service.cs
+++ Spider/Spider/Model/Service.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Spider.Model.BLL;
+using Spider.Model.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,46 @@ namespace Spider.Model
 {
     class Service
     {
+        #region Temperature
+
+        private TemperatureDAL _temperatureDAL;
+
+        private TemperatureDAL TemperatureDAL
+        {
+            get { return _temperatureDAL ?? (_temperatureDAL = new TemperatureDAL()); }
+        }
+
+        public IEnumerable<Temperature> GetTemperatures()
+        {
+            return TemperatureDAL.GetTemperatures();
+        }
+
+        public void InsertTemperature(Temperature temperature)
+        {
+            TemperatureDAL.InsertTemperature(temperature);
+        }
+
+        #endregion
+
+        #region Room
+
+        private RoomDAL _roomDAL;
+
+        private RoomDAL RoomDAL
+        {
+            get { return _roomDAL ?? (_roomDAL = new RoomDAL()); }
+        }
+
+        public IEnumerable<Room> GetRooms()
+        {
+            return RoomDAL.GetRooms();
+        }
+
+        public void UpdateRoom(Room room)
+        {
+            RoomDAL.UpdateRoom(room);
+        }
+
+        #endregion
     }
 }
