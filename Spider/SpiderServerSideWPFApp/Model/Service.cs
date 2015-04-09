7cae60a0ffd75ddde0ea7b6221aef18fd67750cc
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-04-09 11:53:10 +0200

    Insert new temperature received from Arduino to databas working

--- Spider/SpiderServerSideWPFApp/Model/Service.cs
+++ Spider/SpiderServerSideWPFApp/Model/Service.cs
@@ -14,61 +14,61 @@ using System.Runtime.CompilerServices;
 
 namespace SpiderServerSideWPFApp.Model
 {
-    class Service : INotifyPropertyChanged
+    class Service : Domain.Model.Service, INotifyPropertyChanged
     {
-        #region Temperature
+        //#region Temperature
 
-        private TemperatureDAL _temperatureDAL;
+        //private TemperatureDAL _temperatureDAL;
 
-        private TemperatureDAL TemperatureDAL
-        {
-            get { return _temperatureDAL ?? (_temperatureDAL = new TemperatureDAL()); }
-        }
+        //private TemperatureDAL TemperatureDAL
+        //{
+        //    get { return _temperatureDAL ?? (_temperatureDAL = new TemperatureDAL()); }
+        //}
 
-        public IEnumerable<Temperature> GetTemperatures()
-        {
-            return TemperatureDAL.GetTemperatures();
-        }
+        //public IEnumerable<Temperature> GetTemperatures()
+        //{
+        //    return TemperatureDAL.GetTemperatures();
+        //}
 
-        public void InsertTemperature(Temperature temperature)
-        {
-            ICollection<ValidationResult> validationResults;
-            if (!temperature.Validate(out validationResults))
-            {
-                throw new Exception("Objektet klarade inte valideringen.");
-            }
+        //public void InsertTemperature(Temperature temperature)
+        //{
+        //    ICollection<ValidationResult> validationResults;
+        //    if (!temperature.Validate(out validationResults))
+        //    {
+        //        throw new Exception("Objektet klarade inte valideringen.");
+        //    }
 
-            TemperatureDAL.InsertTemperature(temperature);
-        }
+        //    TemperatureDAL.InsertTemperature(temperature);
+        //}
 
-        #endregion
+        //#endregion
 
-        #region Room
+        //#region Room
 
-        private RoomDAL _roomDAL;
+        //private RoomDAL _roomDAL;
 
-        private RoomDAL RoomDAL
-        {
-            get { return _roomDAL ?? (_roomDAL = new RoomDAL()); }
-        }
+        //private RoomDAL RoomDAL
+        //{
+        //    get { return _roomDAL ?? (_roomDAL = new RoomDAL()); }
+        //}
 
-        public IEnumerable<Room> GetRooms()
-        {
-            return RoomDAL.GetRooms();
-        }
+        //public IEnumerable<Room> GetRooms()
+        //{
+        //    return RoomDAL.GetRooms();
+        //}
 
-        public void UpdateRoom(Room room)
-        {
-            ICollection<ValidationResult> validationResults;
-            if (!room.Validate(out validationResults))
-            {
-                throw new Exception("Objektet klarade inte valideringen.");
-            }
+        //public void UpdateRoom(Room room)
+        //{
+        //    ICollection<ValidationResult> validationResults;
+        //    if (!room.Validate(out validationResults))
+        //    {
+        //        throw new Exception("Objektet klarade inte valideringen.");
+        //    }
 
-            RoomDAL.UpdateRoom(room);
-        }
+        //    RoomDAL.UpdateRoom(room);
+        //}
 
-        #endregion
+        //#endregion
 
         #region Serial Communication
 
