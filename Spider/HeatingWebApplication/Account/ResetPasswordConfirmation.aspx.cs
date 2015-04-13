2060642c42a0f391772afcd00d75771c81eee69e
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-04-13 19:49:09 +0200

    New WinForms-project created from template

---
+++ Spider/HeatingWebApplication/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace HeatingWebApplication.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Spider/HeatingWebApplication/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HeatingWebApplication.Startup))]
+namespace HeatingWebApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
