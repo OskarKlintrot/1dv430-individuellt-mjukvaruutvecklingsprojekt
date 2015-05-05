13431b8bc28c844541b672994ed0f0b78fb3d632
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-05-05 22:04:19 +0200

    Typo

--- Spider/HeatingWebApplication/Models/BLL/HistoricalData.cs
+++ Spider/HeatingWebApplication/Models/BLL/HistoricalData.cs
@@ -7,7 +7,6 @@ namespace HeatingWebApplication.Models.BLL
 {
     public class HistoricalData
     {
-        private int _length;
         public string RoomDescription { get; set; }
         public int[] Temperatures { get; set; }
         public string[] Timestamp { get; set; }
